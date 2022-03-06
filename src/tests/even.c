63c8e850b75889a31c0d0dea8756d2b0f95896cf
CesarJZO <cesarjzo@outlook.com>
2022-03-06 17:59:17 -0600

    Add even.c using arguments

---
+++ src/tests/even.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool iseven(int n)
+{
+    return n % 2 == 0;
+}
+
+int main(int argc, char **argv)
+{
+    int n;
+    for (int i = 1; i < argc; i++)
+    {
+        n = atoi(argv[i]);
+        if (iseven(n)) 
+            printf("%d is even\n", n);
+        else
+            printf("%d is odd\n", n);
+    }
+    return 0;
+}
