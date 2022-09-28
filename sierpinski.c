4ee2ac12aa873aa0a8720212f63880a69872ff1c
CesarJZO <cesarjzo@outlook.com>
2022-09-28 16:53:17 -0500

    Add sleep time to sierpinski.c

--- sierpinski.c
+++ sierpinski.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+void printSierpinski(int size);
+
 int main(int argc, char **argv)
 {
-    int n = argc > 1 ? atoi(argv[1]) : 16;
+	int max = argc > 1 ? atoi(argv[1]) : 16;
 
+	for (int i = 2; i <= max; i *= 2)
+	{
+		printSierpinski(i);
+		sleep(1);
+	}
+
+	return 0;
+}
+
+void printSierpinski(int n)
+{
 	for (int y = n - 1; y >= 0; y--)
 	{
-		// printing space till the value of y
 		for (int i = 0; i < y; i++)
-		    printf(" ");
+			printf(" ");
 
-		// printing '*'
 		for (int x = 0; x + y < n; x++)
-		{
 			// printing '*' at the appropriate position
 			// is done by the and value of x and y
 			// wherever value is 0 we have printed '*'
-			if (x & y)
-				printf("  ");
-			else
-				printf("* ");
-		}
-
+			printf(x & y ? "  " : "* ");
 		printf("\n");
 	}
-    return 0;
 }
