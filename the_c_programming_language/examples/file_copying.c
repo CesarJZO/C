2d4a3f7047670fb989082503cf98d5a06d356e29
César Zárate <cesarjzo@outlook.com>
2022-11-08 22:00:51 -0600

    Second version

--- the_c_programming_language/examples/file_copying.c
+++ the_c_programming_language/examples/file_copying.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 /**
- * Copy input to output; 1st version
+ * Copy input to output; 2st version
  */
 main()
 {
     int c;
-    c = getchar();     // read a character
 
-    while (c != EOF)   // while character is not ond-of-file indicator
-    {
-        putchar(c);    // output the character just read
-        c = getchar(); // read a character
-    }
+    while ((c = getchar()) != EOF)
+        putchar(c); 
 }
